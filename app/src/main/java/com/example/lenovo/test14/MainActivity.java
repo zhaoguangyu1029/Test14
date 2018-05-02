d77da0f5451537cf92a7adbfc7752798622749be
zhaoguangyu <1187876466@qq.com>
2018-05-02 20:35:39 +0800

    实践四ZGY

---
+++ app/src/main/java/com/example/lenovo/test14/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.lenovo.test14;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_startSecond = (Button) findViewById(R.id.btn_startSecond);
+        btn_startSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction("lalalalalalala");
+                startActivity(intent);
+            }
+        });
+
+
+        Button btn_call = (Button) findViewById(R.id.btn_Call);
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission("android.permission.CALL_PHONE") !=
+                            PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(MainActivity.this, "缺少打电话权限", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+                String phoneNumber = "15010677118";
+                String encodedPhoneNumber = null;
+                try {
+                    encodedPhoneNumber = URLEncoder.encode(phoneNumber, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + encodedPhoneNumber)));
+            }
+        });
+
+        Button btn_startThird = (Button) findViewById(R.id.btn_startThird);
+        btn_startThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("schemodemo://edu.bistu/path"));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
